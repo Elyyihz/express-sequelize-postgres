84856bbe6d5e7188f336ccb2c62e6dc1e3a54c37
Elyyihz <elynne123lima@gmail.com>
2025-09-28 18:56:13 -0300

    Ajustando erro no deploy

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
+  dialectModule: pg,
 });
 
 export default sequelize;
