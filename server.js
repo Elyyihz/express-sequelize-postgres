9ff5c92a9e2a735a54024d39d78f23ac853525b5
Elyyihz <elynne123lima@gmail.com>
2025-09-28 18:17:09 -0300

    ajustando erros

--- server.js
+++ server.js
@@ -4,15 +4,20 @@ import sequelize from './db.js';
 import userRoutes from './routes/user.js';
 import messageRoutes from './routes/message.js';
 import cors from "cors";
+import path from 'path';
 
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://express-sequelize-postgres.vercel.app/"]
+  origin: ["*","http://localhost:3000", "https://express-sequelize-postgres.vercel.app/"]
 }));
 
 dotenv.config();
 app.use(express.json());
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
+
+app.use(express.static(path.join(path.resolve(), 'public')));
 
 app.use('/users', userRoutes);
 app.use('/messages', messageRoutes);
